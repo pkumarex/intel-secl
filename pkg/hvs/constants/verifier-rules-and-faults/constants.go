de0f5732336c8a6a47f68ca83620abb130333018
Vijayakumar Kumarsamy <VijayakumarX.Kumarasamy@intel.com>
2021-01-03 20:17:24 -0800

    resolve conflicts

--- pkg/hvs/constants/verifier-rules-and-faults/constants.go
+++ pkg/hvs/constants/verifier-rules-and-faults/constants.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Copyright (C) 2020 Intel Corporation
  * SPDX-License-Identifier: BSD-3-Clause
@@ -63,69 +62,3 @@ const (
 	PcrEventLogUnexpectedFields                     = "PcrEventLogUnexpectedFields"
 	PcrEventLogMissingFields                        = "PcrEventLogMissingFields"
 )
-=======
-/*
- * Copyright (C) 2020 Intel Corporation
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-package constants
-
-const PolicyPrefix = "com.intel.mtwilson.core.verifier.policy."
-
-// Verifier Rules
-const (
-	RulePrefix                      = PolicyPrefix + "rule."
-	RuleAikCertificateTrusted       = RulePrefix + "AikCertificateTrusted"
-	RuleAssetTagMatches             = RulePrefix + "AssetTagMatches"
-	RuleFlavorTrusted               = RulePrefix + "FlavorTrusted"
-	RulePcrEventLogEquals           = RulePrefix + "PcrEventLogEquals"
-	RulePcrEventLogIncludes         = RulePrefix + "PcrEventLogIncludes"
-	RulePcrEventLogIntegrity        = RulePrefix + "PcrEventLogIntegrity"
-	RulePcrMatchesConstant          = RulePrefix + "PcrMatchesConstant"
-	RuleTagCertificateTrusted       = RulePrefix + "TagCertificateTrusted"
-	RuleXmlMeasurementsDigestEquals = RulePrefix + "XmlMeasurementsDigestEquals"
-	RuleXmlMeasurementLogEquals     = RulePrefix + "XmlMeasurementLogEquals"
-	RulePcrEventLogEqualsExcluding  = RulePrefix + "PcrEventLogEqualsExcluding"
-	RuleXmlMeasurementLogIntegrity  = RulePrefix + "XmlMeasurementLogIntegrity"
-)
-
-// Verifier Faults
-const (
-	FaultPrefix                                     = PolicyPrefix + "fault."
-	FaultAikCertificateExpired                      = FaultPrefix + "AikCertificateExpired"
-	FaultAikCertificateMissing                      = FaultPrefix + "AikCertificateMissing"
-	FaultAikCertificateNotTrusted                   = FaultPrefix + "AikCertificateNotTrusted"
-	FaultAikCertificateNotYetValid                  = FaultPrefix + "AikCertificateNotYetValid"
-	FaultAllofFlavorsMissing                        = FaultPrefix + "AllOfFlavorsMissing"
-	FaultAssetTagMismatch                           = FaultPrefix + "AssetTagMismatch"
-	FaultAssetTagMissing                            = FaultPrefix + "AssetTagMissing"
-	FaultAssetTagNotProvisioned                     = FaultPrefix + "AssetTagNotProvisioned"
-	FaultFlavorSignatureMissing                     = FaultPrefix + "FlavorSignatureMissing"
-	FaultRequiredFlavorTypeMissing                  = FaultPrefix + "RequiredFlavorTypeMissing"
-	FaultFlavorSignatureNotTrusted                  = FaultPrefix + "FlavorSignatureNotTrusted"
-	FaultFlavorSignatureVerificationFailed          = FaultPrefix + "FlavorSignatureVerificationFailed"
-	FaultPcrEventLogContainsUnexpectedEntries       = FaultPrefix + "PcrEventLogContainsUnexpectedEntries"
-	FaultPcrEventLogInvalid                         = FaultPrefix + "PcrEventLogInvalid"
-	FaultPcrEventLogMissing                         = FaultPrefix + "PcrEventLogMissing"
-	FaultPcrEventLogMissingExpectedEntries          = FaultPrefix + "PcrEventLogMissingExpectedEntries"
-	FaultPcrManifestMissing                         = FaultPrefix + "PcrManifestMissing"
-	FaultPcrValueMismatch                           = FaultPrefix + "PcrValueMismatch"
-	FaultPcrValueMismatchSHA1                       = FaultPcrValueMismatch + "SHA1"
-	FaultPcrValueMismatchSHA256                     = FaultPcrValueMismatch + "SHA256"
-	FaultPcrValueMissing                            = FaultPrefix + "PcrValueMissing"
-	FaultTagCertificateExpired                      = FaultPrefix + "TagCertificateExpired"
-	FaultTagCertificateMissing                      = FaultPrefix + "TagCertificateMissing"
-	FaultTagCertificateNotTrusted                   = FaultPrefix + "TagCertificateNotTrusted"
-	FaultTagCertificateNotYetValid                  = FaultPrefix + "TagCertificateNotYetValid"
-	FaultXmlMeasurementLogContainsUnexpectedEntries = FaultPrefix + "XmlMeasurementLogContainsUnexpectedEntries"
-	FaultXmlMeasurementLogInvalid                   = FaultPrefix + "XmlMeasurementLogInvalid"
-	FaultXmlMeasurementLogMissing                   = FaultPrefix + "XmlMeasurementLogMissing"
-	FaultXmlMeasurementLogMissingExpectedEntries    = FaultPrefix + "XmlMeasurementLogMissingExpectedEntries"
-	FaultXmlMeasurementLogValueMismatchEntries384   = FaultPrefix + "XmlMeasurementLogValueMismatchEntriesSha384"
-	FaultXmlMeasurementsDigestValueMismatch         = FaultPrefix + "XmlMeasurementsDigestValueMismatch"
-	FaultXmlMeasurementValueMismatch                = FaultPrefix + "XmlMeasurementValueMismatch"
-	PcrEventLogUnexpectedFields                     = "PcrEventLogUnexpectedFields"
-	PcrEventLogMissingFields                        = "PcrEventLogMissingFields"
-)
->>>>>>> e89ea91ffdf485e64a2cdbd1deeb06628e9a8ea3
--- pkg/lib/verifier/rules/pcr_matches_constant.go
+++ pkg/lib/verifier/rules/pcr_matches_constant.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Copyright (C) 2020 Intel Corporation
  * SPDX-License-Identifier: BSD-3-Clause
@@ -106,74 +105,3 @@ func (rule *pcrMatchesConstant) Apply(hostManifest *types.HostManifest) (*hvs.Ru
 
 	return &result, nil
 }
-=======
-/*
- * Copyright (C) 2020 Intel Corporation
- * SPDX-License-Identifier: BSD-3-Clause
- */
-package rules
-
-//
-// Rule that compares the 'expected' PCR with the value stored in the host manifest.
-//
-
-import (
-	constants "github.com/intel-secl/intel-secl/v3/pkg/hvs/constants/verifier-rules-and-faults"
-	"github.com/intel-secl/intel-secl/v3/pkg/lib/flavor/common"
-	"github.com/intel-secl/intel-secl/v3/pkg/lib/flavor/model"
-	"github.com/intel-secl/intel-secl/v3/pkg/lib/host-connector/types"
-	"github.com/intel-secl/intel-secl/v3/pkg/model/hvs"
-	"github.com/pkg/errors"
-)
-
-//NewFVPcrMatchesConstant collects the expected PCR values
-func NewFVPcrMatchesConstant(expectedPcr *model.NewFVPcrEx, marker common.FlavorPart) (Rule, error) {
-	if expectedPcr == nil {
-		return nil, errors.New("The expected PCR cannot be nil")
-	}
-
-	if len(expectedPcr.Measurement) < 1 || len(expectedPcr.Measurement) < 1 {
-		return nil, errors.New("The expected PCR cannot have an empty value")
-	}
-
-	rule := pcrMatchesConstant{
-		expectedPcr: *expectedPcr,
-		marker:      marker,
-	}
-	return &rule, nil
-}
-
-type pcrMatchesConstant struct {
-	expectedPcr model.NewFVPcrEx
-	marker      common.FlavorPart
-}
-
-func (rule *pcrMatchesConstant) Apply(hostManifest *types.HostManifest) (*hvs.RuleResult, error) {
-
-	result := hvs.RuleResult{}
-	result.Trusted = true // default to true, set to false in fault logic
-	result.Rule.Name = constants.RulePcrMatchesConstant
-	result.Rule.PCR = &rule.expectedPcr.PCR
-	result.Rule.Measurement = rule.expectedPcr.Measurement
-	result.Rule.PCRMatches = rule.expectedPcr.PCRMatches
-	result.Rule.Markers = append(result.Rule.Markers, rule.marker)
-
-	if hostManifest.PcrManifest.IsEmpty() {
-		result.Faults = append(result.Faults, newPcrManifestMissingFault())
-	} else {
-
-		actualPcr, err := hostManifest.PcrManifest.GetPcrValue(types.SHAAlgorithm(rule.expectedPcr.PCR.Bank), types.PcrIndex(rule.expectedPcr.PCR.Index))
-		if err != nil {
-			return nil, errors.Wrap(err, "Error in getting actual Pcr in Pcr Matches constant rule")
-		}
-
-		if actualPcr == nil || actualPcr.Value == "" || rule.expectedPcr.Measurement == "" {
-			result.Faults = append(result.Faults, newPcrValueMissingFault(types.SHAAlgorithm(rule.expectedPcr.PCR.Bank), types.PcrIndex(rule.expectedPcr.PCR.Index)))
-		} else if rule.expectedPcr.Measurement != actualPcr.Value {
-			result.Faults = append(result.Faults, newPcrValueMismatchFault(types.PcrIndex(rule.expectedPcr.PCR.Index), types.SHAAlgorithm(rule.expectedPcr.PCR.Bank), rule.expectedPcr, *actualPcr))
-		}
-	}
-
-	return &result, nil
-}
->>>>>>> e89ea91ffdf485e64a2cdbd1deeb06628e9a8ea3
