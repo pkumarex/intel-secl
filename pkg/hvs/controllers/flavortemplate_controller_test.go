572fa483bc6f880094181d2b2fc0c56d50ab9716
Maheshwaran S <maheshwaranx.shanmugasundaram@intel.com>
2021-01-11 05:43:52 -0800

    unit test changes

--- pkg/hvs/controllers/flavortemplate_controller_test.go
+++ pkg/hvs/controllers/flavortemplate_controller_test.go
@@ -100,7 +100,7 @@ var _ = Describe("FlavorTemplateController", func() {
 				Expect(err).NotTo(HaveOccurred())
 				w = httptest.NewRecorder()
 				router.ServeHTTP(w, req)
-				Expect(w.Code).To(Equal(https.StatusCreated))
+				Expect(w.Code).To(Equal(http.StatusCreated))
 			})
 		})
 
